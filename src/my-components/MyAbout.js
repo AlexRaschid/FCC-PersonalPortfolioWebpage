095c539a94eb9d2ba036499492892dc2ee7b2494
Alexander Raschid <alexraschid1337@gmail.com>
2022-03-06 13:38:22 -0500

    Made Description box, working button links to socials, base template completed and now major styling and some features here and there will be needed. Project satisfies FCC conditions

--- src/my-components/MyAbout.js
+++ src/my-components/MyAbout.js
@@ -1,12 +1,46 @@
-import { Container, Navbar, Nav, NavDropdown, Row, Col, Button } from 'react-bootstrap';
+import { Container, Navbar, Nav, NavDropdown, Row, Col, Button, Image, ListGroup } from 'react-bootstrap';
 
+//https://aerportfoliobucket.s3.amazonaws.com/MyMainPic.jpg
 
 function MyAbout() {
+  let pfp = "https://aerportfoliobucket.s3.amazonaws.com/MyMainPic.jpg";
     return (
-        <Container>
-
-            
-        </Container>
+        <Row id="MyAbout">
+          
+          <Col xs={4}>
+            <Image fluid roundedCircle src={pfp}></Image>
+          </Col>
+          <Col id="aboutDescription">
+            <Row >
+              <h1 className="text-center">About Me:</h1>
+              <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+              </p>
+            </Row>
+            <Row>
+            <ListGroup className="justify-content-center" horizontal>
+              <ListGroup.Item>
+                <Button id="profile-link" target="_blank" rel="noopener noreferrer" href="https://github.com/AlexRaschid">
+                  <i class="fa-brands fa-github"></i>
+                </Button>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/alexander-raschid/">
+                <i class="fa-brands fa-linkedin-in"></i>
+                </Button>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button target="_blank" rel="noopener noreferrer" href="https://www.freecodecamp.org/alexraschid">
+                <i class="fa-brands fa-free-code-camp"></i>
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+            </Row>
+            <Row>
+              
+            </Row>
+          </Col>
+        </Row>
     );
   }
   
