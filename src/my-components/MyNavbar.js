cd06a0748a2201dde1c1fcd0a9a622e974f5ac12
Alexander Raschid <alexraschid1337@gmail.com>
2022-03-06 13:38:46 -0500

    Added About Me Section

--- src/my-components/MyNavbar.js
+++ src/my-components/MyNavbar.js
@@ -4,10 +4,10 @@ function MyNavbar() {
     return (
         <Navbar id="navbar" bg="light" variant="light" className="bg-transparent" fixed="top">
           <Container>
-          <Navbar.Brand href="#home">Alexander Raschid</Navbar.Brand>
+          <Navbar.Brand >Alexander Raschid</Navbar.Brand>
           <Nav className="ml-auto">
             <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link href="#MyAbout">About Me</Nav.Link>
           </Nav>
           </Container>
         </Navbar>
