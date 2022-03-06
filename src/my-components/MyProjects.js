21fcdcc3e02826cdae0811398b66f1e272e62e3e
Alexander Raschid <alexraschid1337@gmail.com>
2022-03-06 10:21:59 -0500

    Set COntainer to fluid to prevent horizontal screen extension

--- src/my-components/MyProjects.js
+++ src/my-components/MyProjects.js
@@ -1,33 +1,72 @@
 
-import { Container, Navbar, Nav, NavDropdown, Row, Col, Button, Card } from 'react-bootstrap';
+import { useState } from 'react';
+import { ListGroup, Container, Navbar, Nav, NavDropdown, Row, Col, Button, Card } from 'react-bootstrap';
 
 function MyProjects() {
+
+  const [projects, setProjects] = useState([
+        { 
+          id: 1,
+          title: "Attack of the Surfboards",
+          link: "https://alexraschid.github.io/attack_of_the_surfboards/",
+          description: "State Description"
+        },
+        { 
+          id: 2,
+          title: "Jokinator9000",
+          link: "https://alexraschid.github.io/FCC-RandomQuoteMachine/",
+          description: "State Description"
+        },
+        { 
+          id: 3,
+          title: "wordScript",
+          link: "https://rawgit.com/andreGarvin/wordScript/master/index.html",
+          description: "State Description"
+        },
+      ]);
+
+  const createProject = (project) => {
+    return (
+      <Card key={project.id}>
+              
+      <Card.Body className="">
+        <Card.Title>{project.title}</Card.Title>
+        <Card.Text>
+          Blurb
+        </Card.Text>
+        <Row>
+          <Col xs={4}>
+            <ListGroup>
+              <ListGroup.Item>Date</ListGroup.Item>
+              <ListGroup.Item>Role</ListGroup.Item>
+            </ListGroup>
+            <div className="text-center">
+              <Button href={project.link} target="_blank" className="projectButton" variant="primary">Project Link</Button>
+            </div>
+          </Col>
+          
+          <Col>
+          {project.description}
+          </Col>
+        </Row>
+        
+      </Card.Body>
+      <Card.Img variant="top" src="holder.js/100px180" />
+    </Card>
+
+    )
+  }
+
+
     return (
         <Row id="projects">
-            <Col>
+          
 
-            <Card>
-              
-              <Card.Body>
-                <Card.Title>Project</Card.Title>
-                <Card.Text>
-                  Short Context
-                </Card.Text>
-                <Row>
-                  <Col xs={4}>
-                  Useful Info plus a button
-                  </Col>
-                  
-                  <Col>
-                  Description
-                  </Col>
-                </Row>
-                
-              </Card.Body>
-              <Card.Img variant="top" src="holder.js/100px180" />
-            </Card>
+            {projects.map(project => {
+              return createProject(project);
+            })}
 
-            </Col>
+           
         </Row>
     );
   }
